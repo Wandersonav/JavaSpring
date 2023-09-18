7b47fc515dae0e69dde7723ad83414195a49e4ae
wandersonav <wandersonav07@gmail.com>
2023-09-17 21:09:42 -0300

    Salvando todo o projeto

---
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -0,0 +1,53 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
+import br.com.alura.screenmatch.domain.filme.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/filmes")
+public class FilmeController {
+    @Autowired
+    private FilmeRepository repository;
+
+    @GetMapping("/formulario")
+    public String carregaPaginaFormulario(Long id,Model model ) {
+        if (id != null) {
+            var filme = repository.getReferenceById(id);
+            model.addAttribute("filme",filme);
+        }
+        return "filmes/formulario";
+    }
+    @GetMapping
+    public String carregaPaginaListagem(Model model) {
+        model.addAttribute("lista", repository.findAll());
+        return "filmes/listagem";
+    }
+
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados){
+        var filme = new Filme(dados);
+        repository.save(filme);
+        return "redirect:/filmes";
+
+    }
+    @DeleteMapping
+    public String removeFilme(long id){
+        repository.deleteById(id);
+        return "redirect:/filmes";
+
+
+    }
+}
